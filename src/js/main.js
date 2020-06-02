d52948a5256912f91353db4c540d999262965fb8
from0toweb <t9052585903@gmail.com>
2020-06-02 13:24:32 +0300

    feat: add section example with video

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-    $('.slider').slick();
+    // ВИДЕО С ЮТУБА
+    var player;
+    $('.video-wrap__play').on('click',
+        function onYouTubeIframeAPIReady() {
+            player = new YT.Player('player', {
+                height: '100%',
+                width: '100%',
+                videoId: 'VxYh4N1vFOM',
+                events: {
+                    'onReady': videoPlay
+                }
+            });
+        })
+
+    function videoPlay(event) {
+        event.target.playVideo();
+    }
 });
